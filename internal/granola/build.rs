4dcddb0909a0059bf559fc6a15a1838e1c18ee74
Sawangg <leo.mercier1@proton.me>
2025-11-21 16:18:13 +0100

    fix: dynamic api file finding

--- internal/granola/build.rs
+++ internal/granola/build.rs
@@ -1,6 +1,16 @@
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../api/process.proto")?;
-    tonic_build::compile_protos("../../api/vm.proto")?;
-    tonic_build::compile_protos("../../api/maintenance.proto")?;
+    let api_dir = if PathBuf::from("../../api").exists() {
+        "../../api"
+    } else if PathBuf::from("../api").exists() {
+        "../api"
+    } else {
+        panic!("Could not find api directory. Expected at ../../api or ../api");
+    };
+    tonic_build::compile_protos(format!("{}/process.proto", api_dir))?;
+    tonic_build::compile_protos(format!("{}/vm.proto", api_dir))?;
+    tonic_build::compile_protos(format!("{}/maintenance.proto", api_dir))?;
+
     Ok(())
 }
