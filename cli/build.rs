4a51be56cc767b984c12f426c3d2db9173891171
Sawangg <leo.mercier1@proton.me>
2025-11-25 23:14:42 +0100

    chore(deps): update deps

--- cli/build.rs
+++ cli/build.rs
@@ -8,9 +8,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     } else {
         panic!("Could not find api directory. Expected at ../../api or ../api");
     };
-    tonic_build::compile_protos(format!("{}/process.proto", api_dir))?;
-    tonic_build::compile_protos(format!("{}/vm.proto", api_dir))?;
-    tonic_build::compile_protos(format!("{}/maintenance.proto", api_dir))?;
+    tonic_prost_build::compile_protos(format!("{}/process.proto", api_dir))?;
+    tonic_prost_build::compile_protos(format!("{}/vm.proto", api_dir))?;
+    tonic_prost_build::compile_protos(format!("{}/maintenance.proto", api_dir))?;
 
     Ok(())
 }
