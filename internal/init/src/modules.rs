6cd87164e31780eb3ec2ee117947fa0d66b79bb8
Sawang <leo.mercier1@proton.me>
2025-12-18 19:29:38 +0100

    chore(init): remove useless log

--- internal/init/src/modules.rs
+++ internal/init/src/modules.rs
@@ -9,11 +9,6 @@ pub fn load() -> Result<usize, Box<dyn std::error::Error>> {
     let krel = find_kernel_release(modules_base)?;
     let mod_dir = modules_base.join(&krel);
 
-    logging::log(&format!(
-        "Loading kernel modules from {}",
-        mod_dir.display()
-    ));
-
     let alias_db = AliasDb::load(&mod_dir.join("modules.alias"))?;
     let dep_db = DepDb::load(&mod_dir.join("modules.dep"))?;
     let mut loader = ModuleLoader::new(mod_dir);
