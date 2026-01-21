827d52545282245ada51fcc109a53460fe5ecce1
Sawangg <leo.mercier1@proton.me>
2026-01-21 17:14:54 +0100

    feat(stub): using uefi std nightly feature

--- internal/stub/src/main.rs
+++ internal/stub/src/main.rs
@@ -1,5 +1,4 @@
-#![no_std]
-#![no_main]
+#![feature(uefi_std)]
 
 mod loader;
 mod loadfile2;
@@ -7,29 +6,33 @@ mod log;
 mod pe;
 
 use anyhow::{Result, anyhow};
+use std::os::uefi as uefi_std;
 use uefi::Guid;
-use uefi::prelude::*;
+use uefi::Handle;
 use uefi::proto::loaded_image::LoadedImage;
 
 use crate::pe::UkiSections;
 
 const LINUX_INITRD_GUID: Guid = Guid::parse_or_panic("5568e427-68fc-4f3d-ac74-ca555231cc68");
 
-#[entry]
-fn main() -> Status {
-    uefi::helpers::init().unwrap();
-    log_info!("Muak stub v{} starting...", env!("CARGO_PKG_VERSION"));
+/// Performs the necessary setup code for the `uefi` crate.
+fn setup_uefi_crate() {
+    let st = uefi_std::env::system_table();
+    let ih = uefi_std::env::image_handle();
 
-    match run() {
-        Ok(status) => status,
-        Err(e) => {
-            log_error!("Stub error: {}", e);
-            Status::LOAD_ERROR
-        }
+    // Mandatory setup code for `uefi` crate.
+    unsafe {
+        uefi::table::set_system_table(st.as_ptr().cast());
+
+        let ih = Handle::from_ptr(ih.as_ptr().cast()).expect("Something's very wrong");
+        uefi::boot::set_image_handle(ih);
     }
 }
 
-fn run() -> Result<Status> {
+fn main() -> Result<()> {
+    setup_uefi_crate();
+    log_info!("Muak stub v{} starting...", env!("CARGO_PKG_VERSION"));
+
     let image_handle = uefi::boot::image_handle();
 
     let loaded_image = uefi::boot::open_protocol_exclusive::<LoadedImage>(image_handle)
@@ -57,5 +60,7 @@ fn run() -> Result<Status> {
         loader::set_cmdline(kernel_handle, cmdline_bytes)?;
     }
 
-    Ok(loader::start(kernel_handle))
+    let _ = loader::start(kernel_handle);
+
+    Ok(())
 }
